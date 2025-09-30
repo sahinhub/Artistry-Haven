fdeb4eaee7de315a2a7e11aede9f0ed138c006b3
sahincoder <sahincoderbd@gmail.com>
2025-09-30 11:33:10 +0600

    Vercel spreed Insight added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,42 @@ import ThemeToggle from './ThemeToggle';
 
 const Header = () => {
 
-  
+    const navlinks = [
+        { path: '/', label: 'Home' },
+        { path: '/about-me', label: 'About Me' },
+        { path: '/services', label: 'Services' },
+        { path: '/projects', label: 'Projects' },
+        { path: '/contact-me', label: 'Contact Me' },
+    ]
 
     return (
         <div>
-           {/* theme control */}
+            {/* Logo */}
+            <div className="navbar-start flex-1/6">
+                <NavLink to="/" className="text-xl">
+                    <img className="w-10" src="https://i.postimg.cc/DymWhPxY/logo.png" alt="Artistry Haven" />
+                </NavLink>
+            </div>
+
+            {/* Desktop Menu */}
+            <div className="navbar-center hidden justify-center items-center text-center lg:flex flex-3/6 ">
+                <ul className="menu-horizontal px-4 space-x-10 transition-all">
+                    {navlinks.map(link => (
+                        <li key={link.path}>
+                            <NavLink
+                                to={link.path}
+                                className={`
+                                    text-lg menuLink
+                                    `}
+                            >
+                                {link.label}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+            {/* theme control */}
             <ThemeToggle></ThemeToggle>
         </div>
     );
